077ed40c6b000eb4cde1a0d0194996c40a436a43
Ankan Brahmachary <lionsraj05@outlook.com>
2025-01-20 18:25:40 +0530

    Configuring Swagger

--- src/Services/Catalog/Catalog.API/Program.cs
+++ src/Services/Catalog/Catalog.API/Program.cs
@@ -1,4 +1,5 @@
 using Carter;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,10 +12,29 @@ builder.Services.AddMediatR(config =>
 
 // Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options =>
+{
+    options.SwaggerDoc("v1", new OpenApiInfo
+    {
+        Title = "ECommerce Catalog MicroService API",
+        Version = "v1",
+        Description = "API documentation for the ECommerce Catalog MicroService API."
+    });
+});
 
 var app = builder.Build();
 
 //Configure the Http request Pipeline
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI(options =>
+    {
+        options.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog API v1");
+        options.RoutePrefix = string.Empty; // Serve Swagger at the root
+    });
+}
 
 //Map Carter Endpoints
 app.MapCarter();
