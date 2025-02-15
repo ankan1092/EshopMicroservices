2db8b18fc8fe5ea99f4482466e546409b09560d3
ankan1092 <lionsraj05@gmail.com>
2025-02-15 12:24:23 +0530

    get product by category

---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryEndpoint.cs
@@ -0,0 +1,29 @@
+﻿using Carter;
+using Catalog.API.Models;
+using Mapster;
+using MediatR;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryResponse(IReadOnlyList<Product> Products);
+
+    public class GetProductByIdEndpoint : ICarterModule
+    {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapPost("/products/category", async (List<string> categories, ISender sender) =>
+            {
+                var result = await sender.Send(new GetProductByCategoryQuery(categories));
+
+                var response = result.Adapt<GetProductByCategoryResponse>();
+
+                return Results.Ok(response);
+            })
+             .WithName("GetProductByCategory")
+             .Produces<GetProductByCategoryResponse>(StatusCodes.Status200OK)
+             .ProducesProblem(StatusCodes.Status400BadRequest)
+             .WithSummary("Get Product By Category")
+             .WithDescription("Get Product By Category");
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Products/GetProductByCategory/GetProductByCategoryQueryHandler.cs
@@ -0,0 +1,31 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Exceptions;
+using Catalog.API.Models;
+using Marten;
+
+namespace Catalog.API.Products.GetProductByCategory
+{
+    public record GetProductByCategoryQuery(List<string> Categories) : IQuery<GetProductByCategoryResult>;
+    public record GetProductByCategoryResult(IReadOnlyList<Product> Products);
+    internal class GetProductByCategoryQueryHandler
+        (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
+    {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductBycategoryQueryHandler.Handle called with {Query}", query);
+
+            var products = await session.Query<Product>()
+                                    .Where(p => p.Category.Any(c => query.Categories.Contains(c)))
+                                    .ToListAsync(cancellationToken);
+
+
+            if (products.Count==0)
+            {
+                throw new ProductNotFoundException();
+            }
+
+            return new GetProductByCategoryResult(products);
+        }
+    }
+}
