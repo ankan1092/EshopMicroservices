3440776569144b98133a6bd70bc47b6aaef4ed19
ankan1092 <lionsraj05@gmail.com>
2025-09-04 20:14:55 +0530

    Add Entity Framework Core support for SQLite
    Updated `Discount.Grpc.csproj` to include package references for
    `Microsoft.EntityFrameworkCore.Sqlite` and `Microsoft.EntityFrameworkCore.Tools`.
    Modified `appsettings.json` to add a connection string for a SQLite database.
    Introduced `DiscountContext` class in `Class.cs` for managing `Coupon`
    entities with Entity Framework Core.

---
+++ src/Services/Discount/Discount.Grpc/Data/Class.cs
@@ -0,0 +1,17 @@
+﻿using Discount.Grpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Discount.Grpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        DbSet<Coupon> Coupons { get; set; } = default;
+        public DiscountContext(DbContextOptions<DiscountContext>options) 
+            :base(options)
+        {
+
+        }
+        
+    }
+    
+}
