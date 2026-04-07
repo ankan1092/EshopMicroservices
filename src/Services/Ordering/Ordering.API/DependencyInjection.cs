38ad0ebf22928ab1e3048479a3498bf6ce0b7050
ankan1092 <lionsraj05@gmail.com>
2026-04-07 18:04:38 +0530

    Setting up Ordering Domain. Part 1

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApi(this IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication app)
+        {
+            return app;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,10 +1,19 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services to the Container.
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration)
+    .AddApi();
+
+
+
+
 var app = builder.Build();
-
-
-
 
 // configure the HTTP request pipeline.
 
